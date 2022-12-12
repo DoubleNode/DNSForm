31b02f451cd12a8c2711eedf4ef5f49a54ad528e
Darren Ehlers <me@darrenehlers.com>
2022-12-11 21:05:34 -0600

    Updated imageSelectorPlusCell

--- Sources/DNSForm/FieldTypeCells/DNSFormDetailImageSelectorPlusCell.swift
+++ Sources/DNSForm/FieldTypeCells/DNSFormDetailImageSelectorPlusCell.swift
@@ -12,6 +12,7 @@ import DNSBaseStage
 import DNSBaseTheme
 import DNSCore
 import DNSCoreThreading
+import DNSDataObjects
 import DNSProtocols
 import UIKit
 
@@ -28,8 +29,8 @@ open class DNSFormDetailImageSelectorPlusCell: DNSBaseStageCollectionViewCell,
     public typealias Stage = DNSFormStage
     static public let recommendedContentSize = CGSize(width: 414, height: 148)
     
-    var lastURL: URL?
-    
+    var lastMedia: DAOMedia?
+
     public struct Data: Hashable {
         public var autocapitalizationType: UITextAutocapitalizationType = .none
         public var field: String
@@ -46,24 +47,22 @@ open class DNSFormDetailImageSelectorPlusCell: DNSBaseStageCollectionViewCell,
         public var required: Bool
         public var returnKeyType: UIReturnKeyType = .next
         public var style: DNSThemeFieldStyle = .DNSForm.default
-        public var text: String
         public var type: DNSFormFieldType = .none
         public var uppercaseOnly = false
-        public var url: URL?
+        public var media: DAOMedia?
         public var imageAlertMessage: String = ""
         public var textAlertMessage: String = ""
         
-        public init(field: String, formatPattern: String = "", label: String, languageCode: String, placeholder: String, readonly: Bool, required: Bool, text: String, type: DNSFormFieldType = .none, url: URL? = nil) {
+        public init(field: String, formatPattern: String = "", label: String, languageCode: String, media: DAOMedia? = nil, placeholder: String, readonly: Bool, required: Bool, type: DNSFormFieldType = .none) {
             self.field = field
             self.formatPattern = formatPattern
             self.label = label
             self.languageCode = languageCode
+            self.media = media
             self.placeholder = placeholder
             self.readonly = readonly
             self.required = required
-            self.text = text
             self.type = type
-            self.url = url
         }
     }
     public var data: Data? {
@@ -72,12 +71,11 @@ open class DNSFormDetailImageSelectorPlusCell: DNSBaseStageCollectionViewCell,
             guard let data = self.data else {
                 self.deleteButton.isEnabled = false
                 self.selectButton.isEnabled = false
-                
                 self.progressView.setProgress(0.0, animated: false)
                 self.progressView.isHidden = true
                 self.imageView.image = nil
-                self.lastURL = nil
-                
+                self.lastMedia = nil
+
                 self.textField.hideAlert()
                 self.textField.isEnabled = false
                 self.textField.formatPattern = ""
@@ -92,44 +90,10 @@ open class DNSFormDetailImageSelectorPlusCell: DNSBaseStageCollectionViewCell,
             let languageLabel = data.languageCode.isEmpty ? "" : " (\(data.languageCode))"
             self.alertLabel.style = data.style.alertStyle
             self.alertLabel.text = data.imageAlertMessage
-            
+
             self.titleLabel.style = data.style.titleStyle
             self.titleLabel.text = data.label + languageLabel
-            
-            self.deleteButton.isEnabled = !data.readonly
-            self.selectButton.isEnabled = !data.readonly
-            
-            self.progressView.setProgress(0.0, animated: false)
-            self.progressView.isHidden = true
-            
-            let lastString = self.lastURL?.absoluteString ?? ""
-            let string = data.url?.absoluteString ?? ""
-            if let imageUrl = data.url {
-                self.deleteButton.isEnabled = data.readonly ? false : true
-                self.selectButton.isEnabled = false
-                if string != lastString {
-                    self.lastURL = imageUrl
-                    self.progressView.isHidden = false
-                    self.imageView.image = nil
-                    self.imageView.af
-                        .setImage(withURL: imageUrl,
-                                  cacheKey: imageUrl.absoluteString,
-                                  progress: { (progress) in
-                            self.progressView.setProgress(Float(progress.fractionCompleted),
-                                                          animated: true)
-                            self.progressView.isHidden = (progress.fractionCompleted >= 1.0)
-                        },
-                                  imageTransition: UIImageView.ImageTransition.crossDissolve(0.2))
-                }
-            } else {
-                self.deleteButton.isEnabled = false
-                self.selectButton.isEnabled = data.readonly ? false : true
-                if string != lastString {
-                    self.lastURL = nil
-                    self.imageView.image = nil
-                }
-            }
-            
+
             textField.style = data.style
             self.utilityDisplayAlert(data.textAlertMessage, for: textField)
             textField.isEnabled = !data.readonly
@@ -166,10 +130,46 @@ open class DNSFormDetailImageSelectorPlusCell: DNSBaseStageCollectionViewCell,
             }
             textField.placeholder = data.placeholder + languageLabel
             textField.title = data.label + languageLabel
-            
-            if textField.text != data.text {
-                textField.text = data.text
-                self.utilityDisplayAlert(data.textAlertMessage, for: textField)
+
+            if let media = data.media {
+                if textField.text != media.title.asString(for: data.languageCode) {
+                    textField.text = media.title.asString(for: data.languageCode)
+                    self.utilityDisplayAlert(data.textAlertMessage, for: textField)
+                }
+            }
+
+            self.progressView.setProgress(0.0, animated: false)
+            self.progressView.isHidden = true
+
+            let lastUrl = self.lastMedia?.url.asURL(for: data.languageCode)
+            let mediaUrl = data.media?.url.asURL(for: data.languageCode)
+            guard mediaUrl != lastUrl else {
+                self.deleteButton.isEnabled = (mediaUrl != nil)
+                self.selectButton.isEnabled = (mediaUrl == nil)
+                return
+            }
+
+            self.deleteButton.isEnabled = !data.readonly
+            self.selectButton.isEnabled = !data.readonly
+
+            self.lastMedia = data.media
+            self.imageView.image = nil
+            self.progressView.isHidden = false
+            if let mediaUrl {
+                self.deleteButton.isEnabled = data.readonly ? false : true
+                self.selectButton.isEnabled = false
+                self.imageView.af
+                    .setImage(withURL: mediaUrl,
+                              cacheKey: mediaUrl.absoluteString,
+                              progress: { (progress) in
+                        self.progressView.setProgress(Float(progress.fractionCompleted),
+                                                      animated: true)
+                        self.progressView.isHidden = (progress.fractionCompleted >= 1.0)
+                    },
+                              imageTransition: UIImageView.ImageTransition.crossDissolve(0.2))
+            } else {
+                self.deleteButton.isEnabled = false
+                self.selectButton.isEnabled = data.readonly ? false : true
             }
         }
     }
@@ -253,12 +253,14 @@ open class DNSFormDetailImageSelectorPlusCell: DNSBaseStageCollectionViewCell,
             return
         }
         textField.hideAlert()
-        guard textField.text != data.text else { return }
-        let text = textField.text
-        let request = Stage.Models.Field.Request(field: data.field,
-                                                 languageCode: data.languageCode,
-                                                 value: text)
-        changeTextPublisher.send(request)
+        if let media = data.media {
+            guard textField.text != media.title.asString(for: data.languageCode) else { return }
+            let text = textField.text
+            let request = Stage.Models.Field.Request(field: data.field,
+                                                     languageCode: data.languageCode,
+                                                     value: text)
+            changeTextPublisher.send(request)
+        }
     }
 
     // MARK: - Utility methods -
