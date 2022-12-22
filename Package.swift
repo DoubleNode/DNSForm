2ec65fa7754a2bfc1c2cea5d4c4c260ac8653b2d
Darren Ehlers <me@darrenehlers.com>
2022-12-21 21:31:21 -0600

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/DoubleNodeOpen/AnimatedField.git", from: "2.5.33"),
         .package(url: "https://github.com/DoubleNode/DNSAppCore.git", from: "1.10.2"),
-        .package(url: "https://github.com/DoubleNode/DNSBaseStage.git", from: "1.10.14"),
+        .package(url: "https://github.com/DoubleNode/DNSBaseStage.git", from: "1.10.15"),
         .package(url: "https://github.com/DoubleNode/DNSBaseTheme.git", from: "1.10.1"),
         .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.10.27"),
         .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.10.15"),
