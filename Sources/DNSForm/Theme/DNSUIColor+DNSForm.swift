52ef60fabfbd90a2a76ab0c4adaa2855cbb40e2e
Darren Ehlers <me@darrenehlers.com>
2023-04-09 19:22:44 -0500

    Updated field default background disabled colors

--- Sources/DNSForm/Theme/DNSUIColor+DNSForm.swift
+++ Sources/DNSForm/Theme/DNSUIColor+DNSForm.swift
@@ -1036,7 +1036,11 @@ public extension DNSUIColor {
         }
         public enum Field {
             public enum Default {
-                public static var background = DNSUIColor(UIColor.DNSForm.Field.Default.background)
+                public static var background = DNSUIColor(UIColor.DNSForm.Field.Default.Background.normal,
+                                                          disabled: UIColor.DNSForm.Field.Default.Background.disabled,
+                                                          focused: UIColor.DNSForm.Field.Default.Background.focused,
+                                                          highlighted: UIColor.DNSForm.Field.Default.Background.highlighted,
+                                                          selected: UIColor.DNSForm.Field.Default.Background.selected)
                 public static var border = DNSUIColor(UIColor.DNSForm.Field.Default.border)
                 public static var line = DNSUIColor(UIColor.DNSForm.Field.Default.Line.normal,
                                                     disabled: UIColor.DNSForm.Field.Default.Line.disabled)
